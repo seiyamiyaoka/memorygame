73cb8a8b322ada49b8dd5574a813505a2e257460
timeshiftseiya <seiyamiyaoka>
2017-08-17 16:36:03 +0900

    create count function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 let answerResult = [];
+let correctAnswerCount = 0;
 let answerCount = 0;
+
 $(function(){
   prepareSquare()
+  setStar()
+  moveCount()
   openCard()
   reload()
+  pageReload()
 })
 
 
@@ -16,25 +21,34 @@ class Card {
 
 function reload(){
   $('.reload').on('click', function(){
-    console.log(1)
+    $(this.children).addClass('fa-spin')
+    $(this.children).delay(1000).queue(function() {
+    　　$(this).toggleClass('fa-spin').dequeue();
+    });
     answerResult = [];
     answerCount = 0;
+    correctAnswerCount = 0;
     $('.card').remove()
     prepareSquare()
+    $('.evaluation').children().not('.answerTime').removeClass()
+    $('.evaluation').children().not('.answerTime').addClass('fa fa-star-o')
+    $('.answerTime').text(`: ${answerCount} move count`)
   })
 }
 
 function checkAnswer(answers) {
-  console.log(1)
   if (answers[0] === answers[1]){
-    answerCount += 1
+    correctAnswerCount += 1
     answerResult = [];
-    if(answerCount === 8){
+    if(correctAnswerCount === 1){
       $('h2').text("congraturation!!")
+      $('.card-panel').fadeOut()
+      $('.container').last().append($(`<div class="congraturatioin">your clear time ${answerCount}!</div><div class="resetButton"><button type="button" class="btn btn-primary">retry?</button></div>`)).fadeIn()
     }
   } else {
-    answerCount = 0;
+    // answerCount = 0;
     $('.card').each(function(){
+      correctAnswerCount = 0;
       if($(this.children).attr('class').length <= 0){
         $(this.children).delay(1000).queue(function() {
         　　$(this).toggleClass('flip-over').dequeue();
@@ -43,23 +57,12 @@ function checkAnswer(answers) {
       }
     })
   }
-
 }
 
-// let card = {
-//   isOpen: false,
-//   image: "",
-//   open: function() {
-//     if(this.isOpen){
-//       image
-//     }else{
-//       "images/tranp.jpeg"
-//     }
-//   }
-// }
-
-function cardSet() {
-
+function pageReload(){
+  $('.container').on('click', '.btn', function(){
+    location.reload();
+  })
 }
 
 function openCard() {
@@ -69,9 +72,33 @@ function openCard() {
     answer = $(e.target.children[0]).find('img').attr('alt')
     answerResult.push(answer)
     if (answerResult.length === 2){
+      answerCount += 1
+      changeStarColor(answerCount);
+      $('.answerTime').text(`: ${answerCount} move count`)
       checkAnswer(answerResult)
     }
   })
+}
+
+function changeStarColor(count) {
+  let first, second, third;
+  first = $('.evaluation').children().eq(0),
+  second = $('.evaluation').children().eq(1),
+  third = $('.evaluation').children().eq(2)
+  switch(count) {
+    case 5:
+      first.removeClass('fa-star-o')
+      first.addClass('fa-star')
+      break
+    case 10:
+      second.removeClass('fa-star-o')
+      second.addClass('fa-star')
+      break
+    case 15:
+      third.removeClass('fa-star-o')
+      third.addClass('fa-star')
+      break
+  }
 }
 
 function prepareSquare() {
@@ -82,15 +109,27 @@ function prepareSquare() {
     return Math.random() - Math.random()
   })
   mathSize = 16
-
   card_data = []
   for (let size = 1; size <= mathSize; size++) {
     card = new Card();
     card.image = allCard[size -1]
     card_data.push(card)
     $('.card-panel').append(allCard[size -1])
-    console.log(1)
   }
+}
+
+function setStar() {
+  star = '<i class="fa fa-star-o" aria-hidden="true"></i>'
+  let starNumber = 3;
+  for(let time = 0; time < 3; time ++){
+    $('.evaluation').append(star)
+  }
+}
+
+function moveCount(){
+  countHTML = $(`<div class="answerTime">: ${answerCount} move count</div>`)
+  target = $('.evaluation').children().last()
+  target.after(countHTML)
 }
 
 function setCardList() {
